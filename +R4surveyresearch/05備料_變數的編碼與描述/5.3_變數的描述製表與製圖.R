613fe8eb93b7eca4c4d122997179dac918bf3500
Zhou Zhengqing <pkuzzq@gmail.com>
2023-02-26 09:45:19 +0800

    123

---
+++ +R4surveyresearch/05備料_變數的編碼與描述/5.3_變數的描述製表與製圖.R
@@ -0,0 +1,91 @@
+### 《民意調查資料分析的R實戰手冊》
+### 劉正山著．2018五南出版
+### 5.3 變數的描述製表與製圖 
+
+## 用製表的方式描述變數
+library(sjlabelled)
+tscs2013 <- read_data("../tscs2013q2.sav") #讀入原始檔
+
+# table()
+table(tscs2013$v65, exclude=NULL) # exclude=NULL 顯示無效值
+
+# gmodels::CrossTabsle()
+library(gmodels)
+CrossTable(tscs2013$v65)
+
+# sjmisc::frq()
+library(sjmisc)
+frq(tscs2013$v65)
+frq(tscs2013$v65, weights = tscs2013$wr) 
+
+## 清除無效值、重新編碼、排序及上標籤
+tscs2013$v65r <- rec(tscs2013$v65, 
+                     rec="94:99=NA; else=copy",  
+                     as.num = F)
+frq(tscs2013$v65r)
+tscs2013$v65r <- rec(tscs2013$v65r, rec="rev", 
+                     var.label="how serious the issue is", 
+                     val.labels = c("not at all (1)", 
+                                    "not very serious (2)", 
+                                    "serious (3)", 
+                                    "very serious (4)"))
+frq(tscs2013$v65r)
+frq(tscs2013$v65r, weights = tscs2013$wr)  #權數會套用在所有觀察值上（但無效值會歸零則需要與套件作者討論為何會如此）
+
+
+#上半場結束：把含有新增變數的資料檔另存在工作資料夾
+save(tscs2013, file="tscs2013r.rda")  
+
+# psych::describe()
+library(psych)
+# 下半場開始：讀取本書提供的含有編碼後所有變數的資料檔，再接著操作
+load("../tscs2013.rda")  
+describe(tscs2013$age) 
+
+# sjmisc::decr()
+library(sjmisc)
+descr(tscs2013$age) 
+
+## 使用sjPlot為單一變數製圖
+library(sjPlot)
+sjmisc::frq(tscs2013$v65r)
+
+# Mac使用者需設定字體才能正常顯示標籤中的中文字
+sjPlot::set_theme(sjPlot::set_theme(theme.font="PingFang TC"))  
+
+plot_frq(tscs2013$v65r, 
+        weight.by = tscs2013$wr, wrap.title=30)
+
+## 使用sjPlot來視覺化兩個變數之間的關係
+# 方法一：plot_grpfrq()
+library(sjPlot)
+library(sjlabelled)
+frq(tscs2013$sex)
+tscs2013$sex <- set_label(tscs2013$sex, label="sex")  # 變更變數標籤
+tscs2013$sex <- set_labels(tscs2013$sex, labels= c("female", "male")) # 變更選項標籤
+
+tscs2013$v65r <- rec(tscs2013$v65r, rec="rev", 
+                      var.label="how serious the issue is", 
+                      val.labels = c("not at all (1)", 
+                                     "not very serious (2)", 
+                                     "serious (3)", 
+                                     "very serious (4)"))
+
+plot_grpfrq(tscs2013$v65r, tscs2013$sex)  # 橫向比較
+plot_grpfrq(tscs2013$v65r, tscs2013$sex, bar.pos = "stack")  # 堆疊
+
+# 方法二：plot_xtab()
+sjt.xtab(tscs2013$v65r, tscs2013$sex) #製表
+plot_xtab(tscs2013$v65r, tscs2013$sex) #製圖
+
+#下半場結束：把含有新增變數的資料檔另存在工作資料夾
+# 注意：這個檔案與本書所附的檔案已不相同，存在本地資料夾可避免互相覆蓋
+save(tscs2013, file="tscs2013r.rda")  
+
+## 補充盒子：用`ggplot2`畫長條圖
+library(ggplot2)
+load("./wgcoll.rda")
+ggplot(data=wgc, aes(x=factor(c), 
+                     fill=factor(g))) + 
+  geom_bar(width=.6, position="dodge")  
+
---
+++ +R4surveyresearch/07炒與炸_模型的建立與分析結果詮釋/7.4_迴歸模型的比較製圖與製表.R
@@ -0,0 +1,118 @@
+### 《民意調查資料分析的R實戰手冊》
+### 劉正山著．2018五南出版
+### 7.4 迴歸模型的比較、製圖與製表
+
+library(sjPlot)
+load("../wgcoll.rda")
+
+# 線性模型一
+md01 <- lm(aa ~ pe + g, data=wgc)  # 成績 ~ 家長的受教育年數 + 性別
+summary(md01)
+
+# 線性模型二
+md02 <- update(md01, .~. -g)
+plot_model(md02)
+
+# 二元勝算對數模型一（1= 60分過關; 0=沒過關）
+wgc$pass60 <- ifelse(wgc$aa>=60,1,0)  
+md03 <- glm(pass60 ~ pe + g, 
+            family=binomial, data=wgc)
+summary(md03)
+
+# 二元勝算對數模型二 （1= 80分過關; 0=沒過關）
+wgc$pass80 <- ifelse(wgc$aa>=80,1,0)  
+md04 <- update(md01, pass80 ~ . + c)  # 加入城鄉變數(1=郊區；0=都市)
+
+## 模型比較
+# 方法一：analysis of variance (ANOVA)
+# 比較residual sum of squares (RSS)，數值愈小愈好
+anova(md01, md02)  
+
+# 方法二：AIC與BIC 
+# 數值較低的模型較好
+AIC(md01, md02)  # Akaike Information Criterion
+BIC(md01, md02)  # Bayesian Information Criterion
+
+# 方法三：stepAIC
+# 自動挑出AIC最小的模型
+library(MASS)
+stepAIC(md01) # 注意md01必需是個包含全部變數的完整模型（full model）
+
+## 用視覺化方法來比較線性模型
+# 方法一：sjPlot::plot_models()
+library(sjPlot)
+plot_models(md01, md02)
+
+# 方法二：coefplot::multiplot()
+# install.pacakges("coefplot")
+library(coefplot)
+multiplot(md01, md02)
+
+## 用視覺化方法來比較二元勝算對數模型
+library(sjPlot)
+plot_models(md03, md04, colors = "gs") # 使用灰階色系（greyscale）
+
+## 陽春版的表格
+library(sjPlot)
+tab_model(md01) 
+
+## 專業版的表格
+library(sjPlot)
+tab_model(md03, 
+        # 設定要顯示的資訊
+        show.se = TRUE, # 顯示標準誤
+        show.aic = TRUE, # 顯示AIC 
+        show.r2 = TRUE, # 顯示 (pseudo) R-square
+        p.style="asterisk", # 以星號表示顯著程度
+        collapse.ci = FALSE,  # 以獨立欄位顯示信賴區間
+        
+        # 設定小數點的位數
+        digits=3, # 所有估計值的小數點位數
+        digits.p=3, # p值的小數點位數
+        
+        # 線的樣式設計
+        CSS=list(css.topborder="border-top:1px solid black;"), 
+
+        # 欄位重新命名
+        string.pred = "解釋變數",
+        string.ci = "信賴區間",
+        string.se ="標準誤 (S.E.)",
+        string.p="顯著水準 (C.I.)"
+        # file="analysisResult.html" #另存新檔
+        )
+
+
+## 以sjt.glm()製表來比較模型
+tab_model(md03, md04,
+          show.aic=TRUE, 
+          show.ci=FALSE,
+          show.se=TRUE,
+          show.r2 =FALSE,
+          digits.p=3, 
+          digits=3,
+          CSS=list(css.topborder="border-top:1px solid black;")
+          )
+
+### 補充盒子： 為多元及有序勝算對數分析結果製表
+library(stargazer)
+library(VGAM)
+load("../wgcoll.rda")
+wgc$grade <- NA
+wgc$grade[wgc$aa<=100] <- 5 
+wgc$grade[wgc$aa<90] <- 4   
+wgc$grade[wgc$aa<80] <- 3   
+wgc$grade[wgc$aa<70] <- 2  
+wgc$grade[wgc$aa<60] <- 1  
+wgc$grade <- factor(wgc$grade, 
+                    levels= 5:1, 
+                    labels= c("grade A", "grade B", "grade C",
+                              "grade D", "grade F"))
+
+library(nnet)
+mod.nnet <- multinom(grade ~ pe + c, data=wgc)
+
+# 製表
+stargazer(mod.nnet, 
+          type="html", 
+          out="mod.nnet.htm"   # 到資料夾找到並開啟後進行複製貼上
+          )
